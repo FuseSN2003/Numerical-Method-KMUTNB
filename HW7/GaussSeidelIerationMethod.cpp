0d1f25aa66e5e9368dce0a0aa0265c722aa9c58f
FuseSN2003 <sukavit.fuse2003@gmail.com>
2023-09-14 23:00:05 +0700

    Gauss-Seidel Method

--- HW7/GaussSeidelIerationMethod.cpp
+++ HW7/GaussSeidelIerationMethod.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
 #include <cmath>
 
-int main() {
+#define SIZE 4
+
+using namespace std;
+
+void gaussSeidelMethod(double a[][SIZE], double b[], double x[]) {
+  const double epsilon = 1e-3;
+  double xNew[SIZE] = {0.0};
+  bool error;
+  int iteration = 0;
   
+  do {
+    iteration++;
+    for (int i = 0; i < SIZE; i++) {
+      double sum = 0.0;
+      for (int j = 0; j < SIZE; j++) {
+        if (j != i) {
+          sum += a[i][j] * xNew[j];
+        }
+      }
+      xNew[i] = (b[i] - sum) / a[i][i];
+    }
+
+    error = false;
+    for (int i = 0; i < SIZE; i++) {
+      if (fabs((xNew[i] - x[i]) / xNew[i]) * 100 > epsilon) {
+        error = true;
+        break;
+      }
+    }
+    
+    for (int i = 0; i < SIZE; i++) {
+      x[i] = xNew[i];
+    }
+  } while (error);
+  
+  cout << "iteration: " << iteration << endl;
+}
+
+int main() {
+  double a[SIZE][SIZE] = {
+    {5, 2, 0, 0},
+    {2, 5, 2, 0},
+    {0, 2, 5, 2},
+    {0, 0, 2, 5},
+  };
+  double b[SIZE] = {12, 17, 14, 7};
+  double x[SIZE] = {0.0};
+
+  gaussSeidelMethod(a, b, x);
+
+  for (int i = 0; i < SIZE; i++) {
+    cout << "x" << i + 1 << " = " << x[i] << endl;
+  }
   
   return 0;
 }
