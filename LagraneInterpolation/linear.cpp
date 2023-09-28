398135c3541af82f37c44687077a37689aa9596c
FuseSN2003 <sukavit.fuse2003@gmail.com>
2023-09-28 19:03:22 +0700

    newton lagrane

---
+++ LagraneInterpolation/linear.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double lagraneInterpolation(double xValue[], double yValue[], double x, size_t size) {
+  double result = 0.0;
+  for(int i = 0; i < size; i++) {
+    double l = 1.0;
+    for(int j = 0; j < size; j++) {
+      if(j != i) {
+        l *= (xValue[j] - x) / (xValue[j] - xValue[i]);
+      }
+    }
+    result += l * yValue[i];
+  }
+  return result;
+}
+
+int main() {
+  double xValue[] = {0, 80000};
+  double yValue[] = {9.81, 9.5682};
+  double x = 42235;
+
+  size_t size = sizeof(xValue) / sizeof(xValue[0]);
+
+  double result = lagraneInterpolation(xValue, yValue, x, size);
+
+  cout << fixed << setprecision(6) << result << endl;
+
+  return 0;
+}
---
+++ LagraneInterpolation/polynomial.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double dividedDifference(double xValue[], double yValue[], int n) {
+  if (n == 1) {
+    return yValue[0];
+  } else {
+    return (dividedDifference(xValue + 1, yValue + 1, n - 1) - dividedDifference(xValue, yValue, n - 1)) / (xValue[n - 1] - xValue[0]);
+  }
+}
+
+double interpolate(double xValue[], double yValue[], int n, double x) {
+  double result = 0.0;
+  for (int i = 0; i < n; i++) {
+    double term = dividedDifference(xValue, yValue, i + 1);
+    for (int j = 0; j < i; j++) {
+      term *= (x - xValue[j]);
+    }
+    result += term;
+  }
+  return result;
+}
+
+int main() {
+  // x = 0, 20000, 40000, 60000, 80000
+  // y = 9.81, 9.7487, 9.6879, 9.6879 9.5682
+  double xValue[] = {0, 20000, 40000, 60000, 80000};
+  double yValue[] = {9.81, 9.7487, 9.6879, 9.6879, 9.5682};
+  double x = 42235;
+  int n = sizeof(xValue) / sizeof(xValue[0]);
+
+  double result = interpolate(xValue, yValue, n, x);
+
+  cout << fixed << setprecision(6) << result << endl;
+
+  return 0;
+}
